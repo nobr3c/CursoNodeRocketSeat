953b6a8c486ef3bdca64d8e4bf724fd78488a8ed
Nobre 81 <jnobr3c+github@gmail.com>
2025-03-04 13:58:14 -0300

    :construction: Rotas de Criação e listagem (Métodos HTTP)

--- src/server.js
+++ src/server.js
@@ -6,8 +6,31 @@ import http from 'node:http'
 
 // Criar um usuario (name, email, senha)
 
+// - HTTP
+//    - Método HTTP
+//    - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+//GET => Buscar um recurso do Back-end
+//POST => Criar um recurso do Back-end
+//PUT => Atualizar um recurso do Back-end
+//PATCH => Atualizar uma informação especifica de um recurso do Back-end
+
+//GET / users => buscando usuarios no back-end
+//POST / users => criando usuarios no back-end
+
 //arrow fuction função anonima
 const server = http.createServer((req, res) => {
+    const { method, url} = req
+
+    if(method === 'GET' && url === '/users'){
+        return res.end('Listagem de usuários')
+    }
+
+    if(method === 'POST' && url === '/users'){
+        return res.end('Criação de usuários')
+    }
     return res.end('Hello World')
 })
 
